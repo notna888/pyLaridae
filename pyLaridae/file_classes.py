f9d414e57b63024784f842494ca5cda167cac7e3
Anton Savill <anton.savill@gmail.com>
2020-09-02 16:27:36 +0800

    So seems like the code I was writing was fine, was just reusing the internal files regardless of which object I was initialising, this has been fixed now

--- pyLaridae/file_classes.py
+++ pyLaridae/file_classes.py
@@ -1,43 +1,59 @@
 import os
 from os import path
+from urllib.parse import quote as html_escape
 
 
 class FileObj:
 
     name = ''
-    location = ''
-    # total_size = 0.0  # TODO
+    # file_size = 0.0  # TODO
+    # file_type  # TODO - I'm thinking have images/documents/etc
+
+    def __init__(self, name):
+        self.name = name
+
+    def html_name(self):
+        return html_escape(self.name)
 
     def to_html(self):
         tag = f'<li><a href="#">{self.name}</a></li>'
         return tag
 
-    def __init__(self, name, location='.'):
-        self.name = name
-        self.location = location
-
     def __repr__(self):
-        return f'File: {self.location}/{self.name}'
+        return f'File: {self.name}'
 
 
 class FolderObj(FileObj):
 
-    internal_files = []
+    def __init__(self, name):
+        self.name = name
+        self.internal_files = []
 
-    def to_html(self):
-        tag = f'<li><a href="#">{self.name}</a></li>'
+    def this_folder_li_tag(self):
+        return f'<li><a href="/{self.html_name()}">{self.name}</a></li>'
+
+    def to_html(self, depth=None):
+        tag = self.this_folder_li_tag()
         ul_tag = '<ul>'
-        for i, internal_file in enumerate(self.internal_files):
-            # print(i, ':', internal_file)
-            # breakpoint()
-            ul_tag += internal_file.to_html()
+        internal_files = self.internal_files
+
+        if(depth == None):
+            ul_tag += full_depth(internal_files)
+        elif(depth == 0):
+            ul_tag += this_level_html(internal_files)
+        else:
+            new_depth = depth-1
+            for internal_file in internal_files:
+                ul_tag += internal_file.to_html(depth=new_depth)
         ul_tag += '</ul>'
 
         return tag+ul_tag
 
-    def append_files(self, internal_files):
+    def append_files(self, new_files):
         current_files = self.internal_files
-        current_files.extend(internal_files)
+        for file in new_files:
+            current_files.append(file)
+        self.internal_files = current_files
 
     def from_folder_location(folder_location):
         # TODO
@@ -58,9 +74,26 @@ class FolderObj(FileObj):
         return dict_of_files
 
     def __repr__(self):
-        folder_part = f'Folder: {self.location}/{self.name}'
+        folder_part = f'Folder: {self.name}'
         file_count = f'({len(self.internal_files)} files)'
         return folder_part + file_count
+
+
+def full_depth(internal_files):
+    ul_tag = ''
+    for internal_file in internal_files:
+        ul_tag += internal_file.to_html()
+    return ul_tag
+
+
+def this_level_html(internal_files):
+    ul_tag = ''
+    for internal_file in internal_files:
+        if isinstance(internal_file, FolderObj):
+            ul_tag += internal_file.this_folder_li_tag()
+        else:
+            ul_tag += internal_file.to_html()
+    return ul_tag
 
 
 def folder_navigation(folder_location):
@@ -70,19 +103,11 @@ def folder_navigation(folder_location):
     # This would print all the files and directories
     for file_object in files_and_dirs:
         if path.isdir(file_object):
-            this_sub_directory = folder_navigation(file_object)
-            folder = FolderObj(
-                file_object,
-                folder_location
-            )
-            folder.append_files(this_sub_directory)
-            files.append(folder)
+            pass
+            print(f'{file_object} is a directory')
         elif path.isfile(file_object):
-            this_file = FileObj(
-                file_object,
-                folder_location
-            )
-            files.append(this_file)
+            pass
+            print(f'{file_object} is a file')
     return files
 
 
@@ -106,9 +131,9 @@ def fake_folder_test():
     File5 = FileObj('File5')
     File6 = FileObj('File6')
 
-    Folder1 = FolderObj('Folder1')
-    Folder2 = FolderObj('Folder2', 'Folder1')
-    Folder3 = FolderObj('Folder3', 'Folder1/Folder2')
+    Folder1 = FolderObj('Folder 1')
+    Folder2 = FolderObj('Folder2')
+    Folder3 = FolderObj('Folder3')
     Folder4 = FolderObj('Folder4')
 
     Folder1.append_files([File1, Folder2, Folder4])
@@ -116,9 +141,10 @@ def fake_folder_test():
     Folder3.append_files([File4, File5])
     Folder4.append_files([File6])
 
-    print(Folder1.to_html())
+    return Folder1
 
 
 if __name__ == '__main__':
     # real_folder_test()
-    fake_folder_test()
+    top_folder = fake_folder_test()
+    print(top_folder.to_html())
